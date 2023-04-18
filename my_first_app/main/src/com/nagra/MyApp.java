fdde7879797e8408af63bbe0ce390fb8e0ba4b34
Devaiah <devaih.100@gmail.com>
2023-04-18 23:02:33 +0530

    maven project and Dockerfile

--- my_first_app/main/src/com/nagra/MyApp.java
+++ my_first_app/main/src/com/nagra/MyApp.java
@@ -1,7 +1,6 @@
 package com.nagra;
-
 public class MyApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! Hi this is Devaiah");
     }
 }
